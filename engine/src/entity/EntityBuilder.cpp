b79fbc0f75553153cd7d9e8607bb3f8cfedf4ee4
MeyCorentin <corentin.mey@epitech.eu>
2023-10-15 10:23:54 +0200

    [ADD] cpp for entitybuilder

--- engine/src/entity/EntityBuilder.cpp
+++ engine/src/entity/EntityBuilder.cpp
@@ -1,1 +1,15 @@
 #include "entity/EntityBuilder.hpp"
+
+EntityBuilder::EntityBuilder(int arg_id) {
+    Entity* temp = new Entity(arg_id, std::vector<std::shared_ptr<ComponentBase>>());
+    entity_ = std::shared_ptr<Entity>(temp);
+}
+
+EntityBuilder& EntityBuilder::SetID(int arg_id) {
+    entity_->SetId(arg_id);
+    return *this;
+}
+
+std::shared_ptr<Entity> EntityBuilder::Build() {
+    return entity_;
+}
